8dfec54de15f2d63f701f0a7818fbdf1aeb0049c
max <me19@qq.com>
2019-06-10 12:23:26 +0800

    add ssl scripts

---
+++ nginx/ssl_certs/create_ca_cert.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+# 创建CA根证书
+# 非交互式方式创建以下内容:
+# 国家名(2个字母的代号)
+C="CN"
+# 省
+ST="Guangdong"
+# 市
+L="Shenzhen"
+# 公司名
+O="cmzz"
+# 组织或部门名
+OU="paycats"
+# 服务器FQDN或颁发者名
+CN="paycats.cn"
+# 邮箱地址
+emailAddress="demo.paycats.cn"
+
+mkdir -p ./demoCA/{private,newcerts}
+touch ./demoCA/index.txt
+[ ! -f ./demoCA/seria ] && echo 01 > ./demoCA/serial
+[ ! -f ./demoCA/crlnumber ] && echo 01 > ./demoCA/crlnumber
+[ ! -f ./demoCA/cacert.pem ] && openssl req -utf8 -new -x509 -days 36500 -newkey rsa:2048 -nodes -keyout ./demoCA/private/ca_key.pem -out ./demoCA/ca_cert.pem -subj "/C=${C}/ST=${ST}/L=${L}/O=${O}/OU=${OU}/CN=${CN}/emailAddress=${emailAddress}"
+[ ! -f ./demoCA/private/ca.crl ] && openssl ca -crldays 36500 -gencrl -out "./demoCA/private/ca.crl"
