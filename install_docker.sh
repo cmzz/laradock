c86acde110118fc039cfc26fe10c00bf7c5d898c
cmzz <me19@qq.com>
2020-02-23 18:41:32 +0800

    1

--- install_docker.sh
+++ install_docker.sh
@@ -20,8 +20,8 @@ sudo add-apt-repository \
 
 # aliyun 镜像
 sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+curl -fsSL http://mirrors.cloud.aliyuncs.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] http://mirrors.cloud.aliyuncs.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
 
 sudo apt-get -y update 
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
