e4a76b27f55f28d9adb834d13443c712bf8b8261
cmzz <me19@qq.com>
2020-02-25 11:36:15 +0800

    update sh

--- web.tar.sh
+++ web.tar.sh
@@ -13,8 +13,9 @@ rm -rf paycats-mchcp/storage/logs/*.log
 rm -rf paycats-web-new/storage/logs/*.log
 rm -rf payment-system/storage/logs/*.log
 rm -rf ssl100-web/storage/logs/*.log
+rm -rf old_web/storage/logs/*.log
 
-tar zcvf ./static_web/web.tar.gz doukou/ paycats-cp/ paycats-mchapi/ paycats-mchcp/ paycats-web-new/ payment-system/ ssl100-web/
+tar zcvf ./static_web/web.tar.gz doukou/ paycats-cp/ paycats-mchapi/ paycats-mchcp/ paycats-web-new/ payment-system/ ssl100-web/ old_web/
 
 cp -a laradock/nginx ./static_web/
 
