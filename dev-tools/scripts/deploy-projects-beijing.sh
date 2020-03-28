a2d33f8173ccc25140a20d6b59acf034fa5cd1fc
cmzz <me19@qq.com>
2020-03-28 19:07:12 +0800

    update domain

--- dev-tools/scripts/deploy-projects-beijing.sh
+++ dev-tools/scripts/deploy-projects-beijing.sh
@@ -9,7 +9,7 @@ cp -af ${CONFIG_DIR}/nginx/sites/*.conf laradock/nginx/sites/
 cp -af ${CONFIG_DIR}/php-worker/supervisord.d/*.conf laradock/php-worker/supervisord.d/
 
 
-wget http://172.17.56.144/web.tar.gz
+wget http://dl.bj.lanqq.cn/web.tar.gz
 tar xzvf web.tar.gz
 
 # 调整项目配置
